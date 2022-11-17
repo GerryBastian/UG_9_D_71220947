b4a8c1488043404f706b81bbe8706b870ca3fec4
GerryBastian <117911374+GerryBastian@users.noreply.github.com>
2022-11-17 16:36:46 +0700

    Add files via upload

---
+++ 1_D_71220947.py
@@ -0,0 +1,4 @@
+phi = 3.14
+r = int(input("masukan panjang jari-jari lingkaran:"))
+luas = phi*r*r
+print("luas lingkaran adalah  :",str(luas))
---
+++ 2_D_71220947.py
@@ -0,0 +1,6 @@
+s_satu=int(input("masukan angka:"))
+s_dua=int(input("masukan angka:"))
+
+Persegi= s_satu * s_dua
+
+print("luas persegi adalah",persegi)
